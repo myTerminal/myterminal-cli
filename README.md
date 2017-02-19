a9848222403b5fe7f860f9144e97769079a8d9d5
myTerminal <team.terminal@gmail.com>
2017-02-19 15:50:33 +0530

    add files

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # myterminal
-An all-day command-line companion [coming soon]
+
+An all-day command-line companion
+
+[![npm version](https://badge.fury.io/js/myterminal.svg)](https://badge.fury.io/js/myterminal)
+[![npm downloads](https://img.shields.io/npm/dt/myterminal.svg)](https://www.npmjs.com/package/myterminal)  
+[![Build Status](https://travis-ci.org/myTerminal/myterminal.svg?branch=master)](https://travis-ci.org/myTerminal/myterminal)
+[![Code Climate](https://codeclimate.com/github/myTerminal/myterminal.png)](https://codeclimate.com/github/myTerminal/myterminal)
+[![Package Quality](http://npm.packagequality.com/shield/myterminal.svg)](http://packagequality.com/#?package=myterminal)
+[![Coverage Status](https://img.shields.io/coveralls/myTerminal/myterminal.svg)](https://coveralls.io/r/myTerminal/myterminal?branch=master)
+[![bitHound Overall Score](https://www.bithound.io/github/myTerminal/myterminal/badges/score.svg)](https://www.bithound.io/github/myTerminal/myterminal)
+[![bitHound Code](https://www.bithound.io/github/myTerminal/myterminal/badges/code.svg)](https://www.bithound.io/github/myTerminal/myterminal)  
+[![Dependency Status](https://david-dm.org/myTerminal/myterminal.svg)](https://david-dm.org/myTerminal/myterminal)
+[![devDependency Status](https://david-dm.org/myTerminal/myterminal/dev-status.svg)](https://david-dm.org/myTerminal/myterminal#info=devDependencies)
+[![peer Dependency Status](https://david-dm.org/myTerminal/myterminal/peer-status.svg)](https://david-dm.org/myTerminal/myterminal#info=peerDependencies)  
+[![License](https://img.shields.io/badge/LICENSE-GPL%20v3.0-blue.svg)](https://www.gnu.org/licenses/gpl.html)
+[![Gratipay](http://img.shields.io/gratipay/myTerminal.svg)](https://gratipay.com/myTerminal)  
+[![NPM](https://nodei.co/npm/myterminal.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/myterminal/)
+
+## What is it?
+
+*myterminal* is designed to be a command-line application that would help you perform repetitive tasks that you do all day long with the press of a single programmable key. It tends to be an interactive command-line interface that you would (hopefully) keep running in a terminal window on your workstation to perform a set of programmed tasks and present them to the user in a nested manner.
+
+## Installation
+
+*myterminal* is available on *Npm*. You can install it globally with a simple command.
+
+    npm install -g myterminal
+
+## How to Use
+
+Run `myterminal-cli` or simply `myterminal` from the command line passing it a path to a JSON configuration file as shown in the [examples](examples). For example, if the configuration file is stored at your home directory, you can run it as
+
+    myterminal-cli ~/configs.json
+
+*myterminal* will start and the rest should be simple.
+
+## Dependencies
+
+* prompt
+* clear
+
+## To-do
+
+* A lot of things, ideas floating around all over my mind
+
+This is just a start!
---
+++ index.js
@@ -0,0 +1,169 @@
+#!/usr/bin/env node
+
+/* global require module process */
+
+var path = require("path"),
+    stdin = process.stdin,
+    execSync = require("child_process").execSync,
+    clear = require("clear"),
+    prompt = require("prompt"),
+    args = process.argv,
+    configFileRelativePath = args[2],
+    configFilePath = path.resolve(process.cwd(), configFileRelativePath),
+    configs = require(configFilePath);
+
+var myterminalCliCompanion = (function () {
+    var self = this,
+
+        configs,
+
+        currentState = [],
+
+        setConfigs = function (data) {
+            configs = data;
+        },
+
+        showOptions = function () {
+            printCurrentOptions();
+            bindKeyStrokes();
+        },
+
+        printCurrentOptions = function () {
+            var currentCommandBranch = getCurrentCommandBranch();
+
+            clear();
+
+            console.log(currentCommandBranch.title, "\n");
+            getCurrentCommandOptions().forEach(function (k) {
+                console.log(k + ": " + currentCommandBranch.commands[k]["title"]);
+            });
+
+            if (currentCommandBranch !== configs) {
+                console.log("\nq" + ": " + "Go back...\n");
+            } else {
+                console.log("\nq" + ": " + "Quit\n");
+            }
+        },
+
+        getCurrentCommandBranch = function () {
+            return !currentState.length
+                ? configs
+                : currentState.reduce(function (a, c) {
+                    return a["commands"][c];
+                }, configs);
+        },
+
+        getCurrentCommandOptions = function () {
+            return Object.keys(getCurrentCommandBranch().commands);
+        },
+
+        bindKeyStrokes = function () {
+            listenToKeyStrokes();
+            stdin.on("data", keyStrokeHandler);
+        },
+
+        unbindKeyStrokes = function () {
+            stdin.removeListener("data", keyStrokeHandler);
+        },
+
+        listenToKeyStrokes = function () {
+            stdin.setRawMode(true);
+            stdin.resume();
+            stdin.setEncoding("utf8");
+        },
+
+        keyStrokeHandler = function (key) {
+            unbindKeyStrokes();
+
+            if (key === "\u0003") {
+                process.exit();
+            } else if (key === "q") {
+                if (!currentState.length) {
+                    process.exit();
+                }
+                currentState.pop();
+                showOptions();
+            } else if (getCurrentCommandOptions().indexOf(key) > -1) {
+                var selectedCommand = getCommandForOption(key),
+                    task = selectedCommand.task;
+
+                if (task) {
+                    executeCommand(selectedCommand);
+                } else {
+                    currentState.push(key);
+                    showOptions();
+                }
+            } else {
+                showOptions();
+            }
+        },
+
+        getCommandForOption = function (option) {
+            return !currentState.length
+                ? configs.commands[option]
+                : currentState.reduce(function (a, c) {
+                    return a["commands"][c];
+                }, configs).commands[option];
+        },
+
+        executeCommand = function (command) {
+            clear();
+            console.log("Executing command:", command.title, "\n");
+
+            if (!command.params) {
+                executeShellCommand(command.task);
+            } else {
+                gatherParamsAndExecuteCommand(command);
+            }
+        },
+
+        executeShellCommand = function (command) {
+            try {
+                execSync(command, {
+                    stdio: [0, 1, 2]
+                });
+            } catch (e) {
+                // Do not need to do anything particular
+            }
+            bindKeyStrokesToWait();
+        },
+
+        bindKeyStrokesToWait = function () {
+            console.log("\nDone!\nPress any key to continue...");
+            listenToKeyStrokes();
+            stdin.on("data", keyStrokesToWait);
+        },
+
+        unbindKeyStrokesToWait = function () {
+            stdin.removeListener("data", keyStrokesToWait);
+        },
+
+        keyStrokesToWait = function () {
+            unbindKeyStrokesToWait();
+            showOptions();
+        },
+
+        gatherParamsAndExecuteCommand = function (command) {
+            prompt.start();
+            prompt.get(command.params, function(err, result) {
+                var taskToBeExecuted = [
+                    command.task
+                ].concat(command.params.map(function (p, i) {
+                    return result[command.params[i]];
+                })).join(" ");
+
+                executeShellCommand(taskToBeExecuted);
+            });
+        };
+
+    return {
+        setConfigs: setConfigs,
+        showOptions: showOptions
+    };
+})();
+
+prompt.message = "Enter the value for ";
+prompt.delimiter = "";
+
+myterminalCliCompanion.setConfigs(configs);
+myterminalCliCompanion.showOptions();
